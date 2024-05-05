cda51904561e7ef9fa2dbb745b6b84deed77ea17
Tanay <tanay.duddalwar@gmail.com>
2024-05-05 23:06:25 +0530

    update assign-3

--- Assignment-3.cpp
+++ Assignment-3.cpp
@@ -1,49 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-    int lbit;
-    int rbit;
+class ThreadedBinaryTree {
+private:
+    struct Node {
+        int data;
+        Node* left;
+        Node* right;
+        int lbit;
+        int rbit;
+
+        Node(int value) : data(value), left(nullptr), right(nullptr), lbit(0), rbit(0) {}
+    };
+
+    Node* root;
+    Node* head;
+
+    void insert(Node* root, Node* temp);
+    void inorder(Node* head);
+
+public:
+    ThreadedBinaryTree() : root(nullptr), head(nullptr) {}
+
+    void create();
+    void traverseInorder();
 };
 
-Node* temp;
-Node* root = NULL;
-Node* head;
-
-void insert(Node* root, Node* temp);
-void inorder(Node* head);
-
-void create() {
+void ThreadedBinaryTree::create() {
     char ch;
     do {
-        temp = new Node;
-        cin >> temp->data;
-        temp->left = NULL;
-        temp->right = NULL;
-        temp->lbit = 0;
-        temp->rbit = 0;
-        if (root == NULL) {
+        int value;
+        cout << "Enter value to insert: ";
+        cin >> value;
+
+        Node* temp = new Node(value);
+
+        if (root == nullptr) {
             root = temp;
-            head = new Node;
-            head->data = 999;
+            head = new Node(999);
             head->rbit = 1;
             head->lbit = 1;
             head->left = root;
             root->left = head;
             root->right = head;
-        }
-        else {
+        } else {
             insert(root, temp);
         }
-        cout << "Do you want to insert more nodes? ";
+
+        cout << "Do you want to insert more nodes? (Y/N): ";
         cin >> ch;
     } while (ch == 'Y' || ch == 'y');
 }
 
-void insert(Node* root, Node* temp) {
+void ThreadedBinaryTree::insert(Node* root, Node* temp) {
     if (temp->data < root->data) {
         if (root->lbit == 0) {
             temp->left = root->left;
@@ -65,7 +74,7 @@ void insert(Node* root, Node* temp) {
     }
 }
 
-void inorder(Node* head) {
+void ThreadedBinaryTree::inorder(Node* head) {
     Node* t = head->left;
     while (t->lbit != 0) {
         t = t->left;
@@ -86,8 +95,18 @@ void inorder(Node* head) {
     }
 }
 
-int main() {
-    create();
+void ThreadedBinaryTree::traverseInorder() {
+    if (head == nullptr || root == nullptr) {
+        cout << "Tree is empty!" << endl;
+        return;
+    }
     inorder(head);
+}
+
+int main() {
+    ThreadedBinaryTree tree;
+    tree.create();
+    cout << "Inorder Traversal: ";
+    tree.traverseInorder();
     return 0;
 }
